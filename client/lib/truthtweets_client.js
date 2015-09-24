56f99b3bc12c4ce765ba66e11f397eff13507cb5
arebe <rfboyce@gmail.com>
2015-09-24 18:13:28 -0400

    restructured app files

---
+++ client/lib/truthtweets_client.js
@@ -0,0 +1,147 @@
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("tweets");
+
+  Template.body.helpers({
+    tweets: function(){
+      return Tweets.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Meteor.startup(function(){
+    var ar = $('#ar'),
+    canvas = $('canvas', ar)[0],
+    context = canvas.getContext('2d'),
+    video = $('video', ar)[0],
+    navLat, navLong, accurate,
+    hashtag;
+
+
+    context.canvas.width = $(window).width();
+    context.canvas.height = $(window).height();
+    context.font = "20px serif";
+    context.fillStyle = "#3264FF";
+
+    try {
+      if ("geolocation" in navigator){
+        navigator.geolocation.getCurrentPosition(function(position){
+          accurate = (position.coords.accuracy <= 100) ? true : false;
+          navLat = position.coords.latitude;
+          navLong = position.coords.longitude;
+          $("#dynamsg").append('<p>latitude: '+navLat+' longitude: '+navLong+' accuracy: <span id="acc">'+position.coords.accuracy+'</span></p>');
+          console.log("lat: ", navLat, " long: ", navLong, " accuracy: ", position.coords.accuracy);
+          accurate ? $("#acc").css("color", "#2d7317") : $("#acc").css("color", "#73172d");
+        });
+      }
+      else{
+        navLat = 0;
+        navLong = 0;
+      }
+    } catch(err){
+      console.log("geolocation error: ", err);
+      $("#dynamsg").append("<p>geolocation error: ", err,"</p>");
+    }
+
+    try{
+      navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+      window.URL = window.URL || window.mozURL || window.webkitURL;
+
+      // note ab resolution: http://stackoverflow.com/questions/27420581/get-maximum-video-resolution-with-getusermedia
+      navigator.getUserMedia({'video': {
+        optional: [
+        {minWidth: 320},
+        {minWidth: 640},
+        {minWidth: 800},
+        {minWidth: 900},
+        {minWidth: 1024},
+        {minWidth: 1280},
+        {minWidth: 1920},
+        {minWidth: 2560}
+        ]
+      }}, 
+      function(stream){
+        video.src = window.opera ? stream : window.URL.createObjectURL(stream);
+        video.play();
+      }, 
+      function(err){
+        console.errr("video capture error: ", err);
+      })
+
+    } catch(err){
+      console.log("navigator.getUserMedia error: ", err);
+    }
+
+    video.style.position = "absolute";
+    video.style.visibility = "hidden";
+
+    setInterval(function(){
+      var img = context.drawImage(video, 0, 0);
+      renderTweets();
+      //("geolocation" in navigator) ? renderTweets() : renderNoTweets("Please enable geolocation for full AR experience!");
+    }, 100);
+
+    var renderTweets = function(){
+      var tweets = Tweets.find({}, {sort: {createdAt: -1}}).fetch();
+      if(!tweets.length) {
+        console.log("no tweets");
+        return;
+      }
+      tweets.map(function(data){
+        var age = parseInt(Date.now() - data.createdAt);
+        // 14400000 ms == 4 hrs
+        // 3600000 ms == 1 hr
+        // 1200000 ms = 20min
+        // 60000 ms = 1min
+        var ageMax = (1200000),
+        fsizeMax = 50,
+        fsizeMin = 0;
+        if (age > ageMax){ age = ageMax };
+
+        var fsize = Math.floor((((fsizeMin-fsizeMax)*age)/ageMax)+fsizeMax);
+        context.font = fsize+'px sans-serif';
+        context.fillText(data.text, data.xPos, data.yPos);
+      });
+
+    }
+
+    var renderNoTweets = function(message){
+      for(var i = 0; i < 30; i++){
+        context.fillStyle("#f11");
+        context.fillText(message, 10, (10*i));
+      }
+    }
+
+  $("#downloadBtn").click(function(event) {
+    var nameFile = 'fileDownloaded.csv';
+    Meteor.call('download', function(err, fileContent) {
+      if(fileContent){
+        var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, nameFile);
+      }
+    })
+  });
+
+
+  });  // end onstartup
+
+window.ondevicemotion = function(e){
+  var accX = Math.round(e.accelerationIncludingGravity.x*10)/10;
+  var accY = Math.round(e.accelerationIncludingGravity.y*10)/10;
+    // update tweet position with delta movement
+  }
+} // end if meteor.isClient
+
+
+
+
+//********** helpers
+
+function getRandom(min, max) {
+  // inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
---
+++ server/truthtweets_server.js
@@ -0,0 +1,60 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // for sat: #yaminyc
+    var hashtag = "#internet";
+
+    Twit = new TwitMaker({
+      consumer_key:         Meteor.settings.twitter.consumer_key
+      , consumer_secret:      Meteor.settings.twitter.consumer_secret
+      , access_token:         Meteor.settings.twitter.access_token
+      , access_token_secret:  Meteor.settings.twitter.access_token_secret
+    });
+
+    var handleTweets = Meteor.bindEnvironment(function(err, data, response) {
+      console.log(data);
+      console.log(err);
+      for(var i = 0; i < data.statuses.length; i++){
+        Meteor.call("addTweet", data.statuses[i].text);
+      }
+      
+    });
+
+    var handleStream = Meteor.bindEnvironment(function(tweet, err){
+      debugger;
+      console.log("***********************", err, "***********************");
+      console.log("+++++++++++++++++++++++",tweet,"+++++++++++++++++++++++");
+      Meteor.call("addTweet", tweet.text, hashtag);
+    });
+
+
+    var stream = Twit.stream('statuses/filter', { track: hashtag });
+
+    //**************************************************//
+    // ******  uncomment to turn the stream on: ****** //
+    stream.on('tweet', handleStream);
+
+  }); // end onstartup
+
+  Meteor.publish("tweets", function () {
+    return Tweets.find();
+  });
+
+  Meteor.publish("hashtags", function () {
+    return Hashtags.find();
+  });
+
+  Houston.methods("Tweets", {
+    "Download": function(e){
+      var nameFile = 'fileDownloaded.csv';
+      Meteor.call('download', function(err, fileContent) {
+        if(fileContent){
+          var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
+          saveAs(blob, nameFile);
+        }
+      })
+      return "Downloaded Tweets";
+    },
+  });
+
+
+} // end if meteor.isServer
