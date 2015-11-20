ee655e683eb1ef7b055563d4803a3d3be59d6366
arebe <rfboyce@gmail.com>
2015-11-20 03:53:02 -0500

    viewport resizing. closer and closer

--- client/lib/truthtweets_client.js
+++ client/lib/truthtweets_client.js
@@ -11,6 +11,8 @@ if (Meteor.isClient) {
     y: 0.0,
   };
 
+  var vid = {w: 0, h: 0}
+
   Template.body.helpers({
     tweets: function(){
       return Tweets.find({}, {sort: {createdAt: -1}});
@@ -72,6 +74,10 @@ if (Meteor.isClient) {
       function(stream){
         video.src = window.opera ? stream : window.URL.createObjectURL(stream);
         video.play();
+        video.addEventListener("loadedmetadata", function(e){
+          vid.w = this.videoWidth;
+          vid.h = this.videoHeight;
+        })
       }, 
       function(err){
         console.err("video capture error: ", err);
@@ -85,78 +91,108 @@ if (Meteor.isClient) {
     video.style.visibility = "hidden";
 
     setInterval(function(){
-      // var x, y, 
-      // w = $(window).width(), 
-      // h = $(window).height();
-      // rat = video.height / video.width;
-      // // detect if portrait or landscape
-      // if($(window).width() > $(window).height()){
-      //   // it's landscape!
-      //   x = 0;
-      //   y = video.height > $(window).height() ? (video.height - $(window).height())/2 : 0;
-
-      // }
-      // else{
-      //   // it's portait! 
-      //   x = video.width > $(window).width() ? (video.width - $(window).width())/2 : 0;
-      //   y = 0;
-
+      var x=0, y=0, w, h;
+      
+      // var scale = {x: 1, y: 1};
+      // // scale > 1 if viewport dim is larger
+      // console.log("window.width: "+ $(window).width(), " window.height: ", $(window).height());
+      // console.log("video.width: ", vid.w, " video.height: ", vid.h);
+      // scale.x = $(window).width() / vid.w; 
+      // scale.y = $(window).height() / vid.h;
+      // console.log("video scale: ", scale);
+      // if (scale.x < 1 || scale.y < 1) {
+      //   // if canvas is larger (on either dim), no scaling needed
+      //   w = vid.w; 
+      //   h = vid.h;
+      // } else if (scale.x < scale.y) {  
+      //   // if landscape, use x scale
+      //   w = vid.w * scale.x;
+      //   h = vid.h * scale.x;
+      // } else {
+      //   // if portrait, use y scale
+      //   w = vid.w * scale.y;
+      //   h = vid.h * scale.y;
       // }
 
       // detect if video dims are larger than viewport dims
-      // if ((video.width - $(window).width()) > (video.height - $(window).height())) {
-      //   // portrait
-      //   x = (video.width - $(window).width())/2;
-      //   y = 0;
-      //   w = video.width * ($(window).height()/video.height);
-      //   h = $(window).height();
-      // }
-      // else {
-      //   // landscape
-      //   x = 0;
-      //   y = (video.height - $(window).height())/2;
-      //   w = $(window).width();
-      //   h = video.height * ($(window).width()/video.width);
-      // }
-      var img = context.drawImage(video, 0, 0);
+      var scale = {x: 1, y: 1};
+      scale.x = vid.w / $(window).width(); 
+      scale.y = vid.h / $(window).height();
+      console.log("window.width: "+ $(window).width(), " window.height: ", $(window).height());
+      console.log("video.width: ", vid.w, " video.height: ", vid.h);
+      console.log("scale.x: ", scale.x, " scale.y: ", scale.y);
+      if ((vid.w - $(window).width()) > (vid.h - $(window).height())) {
+        // portrait
+        y = 0;
+        if(scale.y < 1){
+          x = (vid.w - $(window).width())/2;
+          w = vid.w * scale.y;
+          h = vid.h * scale.y;          
+        }
+        else{
+          x = 0;
+          w = vid.w ;
+          h = vid.h ;           
+        }
+
+      }
+      else {
+        // landscape
+        x = 0;
+        if(scale.x < 1){
+          y = (vid.h - $(window).height())/2;
+          w = vid.w * scale.x;
+          h = vid.h * scale.x;          
+        }
+        else{
+          y = 0;
+          w = vid.w ;
+          h = vid.h ;           
+        }
+      }
+
+      var img = context.drawImage(video, x, y, w, h, 0, 0, $(window).width(), $(window).height());
       renderTweets();
       //("geolocation" in navigator) ? renderTweets() : renderNoTweets("Please enable geolocation for full AR experience!");
     }, 100);
 
-// from https://gist.github.com/zachstronaut/1184900
-  window.addEventListener(
-    'load',
-    function () {
-      var canvas = document.getElementsByTagName('canvas')[0];
+//***+++ from https://gist.github.com/zachstronaut/1184900   +++***//
+window.addEventListener(
+  'load',
+  function () {
+    var canvas = document.getElementsByTagName('canvas')[0];
 
-      fullscreenify(canvas);
-    },
-    false
-    );
+    fullscreenify(canvas);
+  },
+  false
+  );
 
-  function fullscreenify(canvas) {
-    var style = canvas.getAttribute('style') || '';
-    
-    window.addEventListener('resize', function () {resize(canvas);}, false);
+function fullscreenify(canvas) {
+  var style = canvas.getAttribute('style') || '';
 
-    resize(canvas);
+  window.addEventListener('resize', function () {resize(canvas);}, false);
 
-    function resize(canvas) {
-      var scale = {x: 1, y: 1};
-      scale.x = (window.innerWidth - 10) / canvas.width;
+  resize(canvas);
+
+  function resize(canvas) {
+    var scale = {x: 1, y: 1};
+      // scale > 1 if viewport dim is larger
+      scale.x = (window.innerWidth - 10) / canvas.width; 
       scale.y = (window.innerHeight - 10) / canvas.height;
-
+      //console.log("window.innerHeight: ", window.innerHeight - 10, " canvas.height: ", canvas.height);
+      //console.log("scale: ", scale);
       if (scale.x < 1 || scale.y < 1) {
-        scale = '1, 1';
-      } else if (scale.x < scale.y) {
-        scale = scale.x + ', ' + scale.x;
+        scale = '1, 1'; // if canvas is larger (on either dim), no scaling needed
+      } else if (scale.x < scale.y) {  
+        scale = scale.x + ', ' + scale.x;  // if landscape, use x scale
       } else {
-        scale = scale.y + ', ' + scale.y;
+        scale = scale.y + ', ' + scale.y;  // if portrait, use y scale
       }
 
       canvas.setAttribute('style', style + ' ' + '-ms-transform-origin: center top; -webkit-transform-origin: center top; -moz-transform-origin: center top; -o-transform-origin: center top; transform-origin: center top; -ms-transform: scale(' + scale + '); -webkit-transform: scale3d(' + scale + ', 1); -moz-transform: scale(' + scale + '); -o-transform: scale(' + scale + '); transform: scale(' + scale + ');');
     }
   }
+  //***+++  ----------------------------------------------  +++***//
 
   var renderTweets = function(){
     var tweets = Tweets.find({}, {sort: {createdAt: -1}}).fetch();
